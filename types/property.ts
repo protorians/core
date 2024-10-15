bbf379a5a13559b1382e323669923373c7e6f579
Protorians <protorians.dev@gmail.com>
2024-10-15 13:29:18 +0000

    Fix Property type

--- types/property.ts
+++ types/property.ts
@@ -1,4 +1,3 @@
-
 export type IPropertySetterProps<T extends IPropertyScheme, P extends keyof T> = {
   target: T,
   prop: P,
@@ -16,23 +15,39 @@ export type IPropertySetter<T extends IPropertyScheme, P extends keyof T> = (pro
 export type IPropertyGetter<T extends IPropertyScheme, P extends keyof T> = (props: IPropertyGetterProps<T, P>) => T[P];
 
 export type IPropertyScheme = {
-  [P: string]: object | string | number | null | undefined;
+  [P: string]: string | number | boolean | null | undefined | Symbol | BigInt
 }
 
 export type IPropertyEachCallback<T extends IPropertyScheme> = (value: T[keyof T], key: keyof T, map: Map<keyof T, T[keyof T]>) => void
 
 export interface IProperty<T extends IPropertyScheme> {
   get map(): Map<keyof T, T[keyof T]>;
-  get scheme():  IterableIterator<[keyof T, T[keyof T]]>;
+
+  get scheme(): IterableIterator<[keyof T, T[keyof T]]>;
+
   get values(): IterableIterator<T[keyof T]>;
+
   get keys(): IterableIterator<keyof T>;
-  each(callback: IPropertyEachCallback<T>): IProperty<T>
+
+  setter(setter: IPropertySetter<T, keyof T>): this;
+
+  getter(getter: IPropertyGetter<T, keyof T>): this;
+
+  each(callback: IPropertyEachCallback<T>): IProperty<T>;
+
   set<P extends keyof T>(name: P, value: T[P]): IProperty<T>;
+
   get<P extends keyof T>(name: P): T[P];
+
   exist<P extends keyof T>(key: P): boolean;
+
   fill(scheme: T): IProperty<T>;
+
   reset(): IProperty<T>;
+
   clear(): IProperty<T>;
+
   delete<P extends keyof T>(key: P): IProperty<T>;
+
   export(): T;
 }
