8a02420cd50fd319c86b7b963b1fcf74d6e8c9cd
Protorians <protorians.dev@gmail.com>
2024-10-19 23:28:54 +0000

    Add slugify function to utilities

--- source/utilities/text.ts
+++ source/utilities/text.ts
@@ -55,3 +55,19 @@ export function fixExponent(x: number) {
   return value;
 }
 
+export function slugify(str: string): string {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  const from = "ãàáäâáº½èéëêìíïîõòóöôùúüûñç·/_,:;";
+  const to = "aaaaaeeeeeiiiiooooouuuunc------";
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-');
+
+  return str;
+}
