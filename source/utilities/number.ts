fd1be31fdcaa8d2d3cfb35f78d5d6050f328acab
Protorians <protorians.dev@gmail.com>
2025-03-13 11:18:21 +0000

    Add `adjustPercent`, `adjustDecimalPercent`, `pad` and `clamp` in  number utilities

--- source/utilities/number.ts
+++ source/utilities/number.ts
@@ -1,3 +1,19 @@
 export function isNumber(number: string | number): boolean {
-    return !isNaN(parseInt(`${number}`) * 1);
+    return !isNaN(parseInt(`${number}`));
+}
+
+export function adjustPercent(value: number) {
+    return value < 0 ? 0 : (value > 100 ? 100 : value);
+}
+
+export function adjustDecimalPercent(value: number) {
+    return value < 0 ? 0 : (value > 1 ? 1 : value);
+}
+
+export function pad(numeric: string, length: number = 2): string {
+    return (new Array(length).join('0') + numeric).slice(-length);
+}
+
+export function clamp(value: number, min: number, max: number): number {
+    return Math.min(Math.max(value, min), max);
 }
