5565bea30f5af76d4608e5af6eeecb30adfd83ca
Protorians <protorians.dev@gmail.com>
2025-03-13 11:20:29 +0000

    Add `stripHTMLTags`, `ucFirstLetter`, `lcFirstLetter`, `trimSpace`, `ucWords`, `randomName`, `logTime`, `logDateTime` in text utilities

--- source/utilities/text.ts
+++ source/utilities/text.ts
@@ -24,6 +24,10 @@ export function stripSlashes(text: string) {
   return text.replace(new RegExp("\\'", 'g'), "'")
 }
 
+export function stripHTMLTags(text: string): string {
+  return text.replace(/<\/?[^>]+(>|$)/g, "");
+}
+
 export function unCamelCase(value: string, separator = '-'): string {
   return value.replace(/[A-Z]/g, letter => `${separator}${letter.toLowerCase()}`);
 }
@@ -66,4 +70,52 @@ export function slugify(str: string): string {
     .replace(/-+/g, '-');
 
   return str;
+}
+
+export function ucFirstLetter(value: string) {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
+export function lcFirstLetter(value: string) {
+  return value.charAt(0).toLowerCase() + value.slice(1);
+}
+
+export function trimSpace(data: string) {
+  return data.replace(/ /g, '')
+}
+
+export function ucWords(data: string, strict: boolean = false): string {
+  return data.split(' ').map(frag => frag.charAt(0).toUpperCase() + (strict ? frag.slice(1).toLowerCase() : frag.slice(1))).join(' ');
+}
+
+export function randomName(length: number = 10) {
+  const provider = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let out: string[] = [];
+
+  for (let index = 0; index < length; index++) {
+    out.push(provider.charAt(Math.floor(Math.random() * provider.length)))
+  }
+
+  return out.join('');
+}
+
+export function logTime(date?: Date) {
+  return (date || (new Date())).toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  });
+}
+
+export function logDateTime(date?: Date) {
+  return (date || (new Date())).toLocaleTimeString('en-US', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  });
 }
